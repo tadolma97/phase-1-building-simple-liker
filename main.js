24d8f5443fa4441818eacbbff46d02c32bd3e459
Tashi Dolma <tadolma97@gmail.com>
2022-05-11 14:32:03 -0400

    commit tree 3b900277d68b8d8afaf5d5e6102b2a5ef067c280

--- main.js
+++ main.js
@@ -3,9 +3,30 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const heartButton=document.querySelectorAll('.like-glyph')
 
+heartButton.forEach(glyph=>glyph.addEventListener('click', functionTodo))
 
-
+function functionTodo(e){
+  heart=e.target
+  mimicServerCall('randomURL')
+  .then(function(){
+    if (heart.innerText===EMPTY_HEART){
+      heart.innerText=FULL_HEART
+      heart.className="activated-heart"
+    }
+    else {
+      heart.innerText=EMPTY_HEART
+      heart.className=''
+    }
+  })
+  .catch(()=>{
+    const modal=document.getElementById('modal')
+    modal.className=''
+    modal.innerText='Error'
+    setTimeout(()=>{modal.className='hidden'}, '3000')
+  })
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
